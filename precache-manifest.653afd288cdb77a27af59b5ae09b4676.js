19a6a088025e9456411572be91582cc5abf847e8
Alvin Hager <alvinhager@Alvins-Air.lan>
2020-01-25 17:44:37 +0100

    Updates

---
+++ precache-manifest.653afd288cdb77a27af59b5ae09b4676.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3ddee7d0003fb21cdb51e415fff1e44",
+    "url": "/AlvinProject1/index.html"
+  },
+  {
+    "revision": "3ab2318d4d9e4ae63b6f",
+    "url": "/AlvinProject1/static/css/main.2bc1c1f5.chunk.css"
+  },
+  {
+    "revision": "27a72aedd70dd327b250",
+    "url": "/AlvinProject1/static/js/2.f8813e03.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/AlvinProject1/static/js/2.f8813e03.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3ab2318d4d9e4ae63b6f",
+    "url": "/AlvinProject1/static/js/main.bb27bb00.chunk.js"
+  },
+  {
+    "revision": "6256ec7c6c165be8aab7",
+    "url": "/AlvinProject1/static/js/runtime-main.64482272.js"
+  },
+  {
+    "revision": "098c789055e4dcff6df04ac79d1c989f",
+    "url": "/AlvinProject1/static/media/Ikaros-Regular.098c7890.otf"
+  }
+]);
